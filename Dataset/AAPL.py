172810ff01bcbe9cf8fca620ca20707a621b22ae
JDiTallo <ditallo.jack@gmail.com>
2025-07-07 17:59:18 -0400

    Converted yfinance data to csv, and created code utilizing such
    This is useful for training the model without the internet, but it may also be useful when adding additional features in the separate file.

---
+++ Dataset/AAPL.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+
+import pandas as pd
+import numpy as np
+
+from datetime import date
+
+#Parameters
+symbol = "AAPL"
+
+data_period = "10y"
+data_interval = "1d" #How precise is the data being measured is
+
+ticker = yf.Ticker(symbol)
+
+historical_data = ticker.history(period=data_period, interval=data_interval)
+historical_data = historical_data[["Open", "High", "Low", "Close"]]
+
+historical_data.reset_index(inplace=True)
+
+historical_data.to_csv("Dataset/APPL.csv", index=False)
