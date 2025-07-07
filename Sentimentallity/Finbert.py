a9be227304db5e8cc76675199aaaae85c0f0cdf8
JDiTallo <ditallo.jack@gmail.com>
2025-07-07 17:14:56 -0400

    Added Averages to Finbert.py
    This allows for the program to find the average values for the positive, negative, and neutral values for each comment.

--- Sentimentallity/Finbert.py
+++ Sentimentallity/Finbert.py
@@ -8,10 +8,12 @@ model_name = "ProsusAI/finbert"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
-# Example text of the model
-text = [
+# Example text for the model
+comments = [
     "Apple's stocks are soaring today, it looks like its a good time to sell.",
+
     "It doesn't look good for Apple, the outlook doesn't look good.",
+
     "People are very much loving Apple as a company, showing a huge increase in sales."
 
     ]
@@ -26,8 +28,39 @@ pipe = pipeline(
     device=0 if torch.cuda.is_available() else -1
 )
 
-for i in text:
-    results = pipe(i)
+# Sums to calculate averages
+positive_sum = 0
+negative_sum = 0
+neutral_sum = 0
+
+print()
+
+for text in comments:
+    results = pipe(text)
     for score in results[0]:
         print(f"{score["label"]}: {score["score"]}")
+
+        # Selects label of the score: Positive, Negative, or Neutral
+        score_label = score["label"]
+        
+        # Similar to a "switch" statement
+        # Here, label is determined as being either positive, negative, or neutral
+        # Afterwards, the value of the score is added to the sum
+        match score_label: 
+            case "positive":
+                positive_sum += score["score"]
+            case "negative":
+                negative_sum += score["score"]
+            case "neutral":
+                neutral_sum = score["score"]
     print()
+
+# Averages for each category
+positive_avg = positive_sum / len(comments)
+negative_avg = negative_sum / len(comments)
+neutral_avg = neutral_sum / len(comments)
+
+print(f"Averages from the {len(comments)} comments:")
+print(f"Positive: {positive_avg}")
+print(f"Negative: {negative_avg}")
+print(f"Neutral: {neutral_avg}")
